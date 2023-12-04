de03bf0f240343561880e63e9bda3dbbccb58806
Valentin <vgeadau@yahoo.com>
2023-12-05 01:10:52 +0200

    Added deadlock example

---
+++ src/DeadlockExample.java
@@ -0,0 +1,43 @@
+public class DeadlockExample {
+    private static final Object resource1 = new Object();
+    private static final Object resource2 = new Object();
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            synchronized (resource1) {
+                System.out.println("Thread 1: Holding resource 1");
+
+                try {
+                    Thread.sleep(100); // Simulating some work
+                } catch (InterruptedException e) {
+                    // handle error if needed
+                }
+
+                System.out.println("Thread 1: Waiting for resource 2");
+                synchronized (resource2) {
+                    System.out.println("Thread 1: Holding resource 1 and resource 2");
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            synchronized (resource2) {
+                System.out.println("Thread 2: Holding resource 2");
+
+                try {
+                    Thread.sleep(100); // Simulating some work
+                } catch (InterruptedException e) {
+                    // handle error if needed
+                }
+
+                System.out.println("Thread 2: Waiting for resource 1");
+                synchronized (resource1) {
+                    System.out.println("Thread 2: Holding resource 1 and resource 2");
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+}
